3d76653ed8619e353dfac7459b2ea21b052e1261
Ankit Yadav <ankityadav943@gmail.com>
2021-08-27 15:16:28 +0530

    newsItem is populated from api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
+  
+
     render() {
+        let { title, description,imageUrl, newsUrl } = this.props;
         return (
             <div>
-                NewsItem
+              <div className="card my-3" style={{ width: '18rem' }}>
+                 <img src={imageUrl?imageUrl:"https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg"} style={{height: '30vh'}} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                        <h5 className="card-title">{ title}...</h5>
+                        <p className="card-text">{ description}...</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+            </div>
+            </div>
             </div>
         )
     }
