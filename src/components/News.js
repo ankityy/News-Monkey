37c0fd2137b7b1514419940b9a66193c06c4615f
Ankit Yadav <ankityadav943@gmail.com>
2021-08-27 16:12:22 +0530

    page navigation is done to show all news

--- src/components/News.js
+++ src/components/News.js
@@ -5,19 +5,44 @@ export class News extends Component {
 
     constructor(){
         super();
-        console.log("i am a constructor from news component");
         this.state = {
             articles : [],
-            loading : false
+            loading: false,
+            page:1
         }
     }
     
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ed48ec967f3a4fad82d2481766ad1d5e";
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ed48ec967f3a4fad82d2481766ad1d5e&page=1&pageSize=20";
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
         this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults
+        })
+    }
+
+    handleNext =async () => {
+
+        if (this.state.page+1 <= Math.ceil(this.state.totalResults / 20)) {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ed48ec967f3a4fad82d2481766ad1d5e&page=${this.state.page+1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+            page: this.state.page + 1 ,
+            articles: parsedData.articles
+         })    
+        }
+        
+    }
+
+    handlePrev = async () => {
+       
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ed48ec967f3a4fad82d2481766ad1d5e&page=${this.state.page-1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            page: this.state.page - 1,
             articles: parsedData.articles
         })
     }
@@ -36,6 +61,11 @@ export class News extends Component {
                         </div>
                     })}
                    
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} className="btn btn-dark btn-sm" onClick={this.handlePrev}>&larr; Previous</button>
+                    <p style={{color: "blue"}}>Page - {this.state.page} </p>
+                    <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults / 20) } className="btn btn-dark btn-sm" onClick={this.handleNext}>Next &rarr;</button>
                 </div>
             </div>
         )
